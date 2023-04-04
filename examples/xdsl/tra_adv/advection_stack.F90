e2023b9542e64f58eedf31f4848c48eec92352a8
Nick Brown <nick@mesham.com>
2023-04-04 15:06:45 +0100

    Advection benchmarks

---
+++ examples/xdsl/tra_adv/advection_stack.F90
@@ -0,0 +1,48 @@
+subroutine main()
+    real*8, dimension(64,64,64) :: su, sv, sw, u, v, w
+    real*8, dimension(64) :: tzc1, tzc2, tzd1, tzd2
+    integer :: k, j, i, nx, ny, nz
+    
+    nx=64
+    ny=64
+    nz=64
+    
+    do i=1, nx
+      do j=1, ny
+        do k=1, nz
+          u(k,j,i)=10.0
+          v(k,j,i)=20.0
+          w(k,j,i)=30.0
+        end do
+      end do
+    end do
+    
+    do k=1, nz
+      tzc1(k)=50.0
+      tzc2(k)=15.0
+      tzd1(k)=100.0
+      tzd2(k)=5.0
+    end do
+    
+
+    do i=2,nx-1
+      do j=2,ny-1
+        do k=2,nz-1
+          su(k, j, i)=&
+            (2.0*(u(k, j, i-1)*(u(k, j, i)+u(k, j, i-1))-u(k, j, i+1)*(u(k, j, i)+u(k, j, i+1)))) + &
+            (1.0*(u(k, j-1, i)*(v(k, j-1, i)+v(k, j-1, i+1))-u(k, j+1, i)*(v(k, j, i)+v(k, j, i+1)))) + &
+            (tzc1(k)*u(k-1, j, i)*(w(k-1, j, i)+w(k-1, j, i+1))-tzc2(k)*u(k+1, j, i)*(w(k, j, i)+w(k, j, i+1)))
+
+          sv(k, j, i)=&
+            (2.0*(v(k, j-1, i)*(v(k, j, i)+v(k, j-1, i))-v(k, j+1, i)*(v(k, j, i)+v(k, j+1, i)))) + &
+            (2.0*(v(k, j, i-1)*(u(k, j, i-1)+u(k, j+1, i-1))-v(k, j, i+1)*(u(k, j, i)+u(k, j+1, i)))) + &
+            (tzc1(k)*v(k-1, j, i)*(w(k-1, j, i)+w(k-1, j+1, i))-tzc2(k)*v(k+1, j, i)*(w(k, j, i)+w(k, j+1, i)))
+
+          sw(k, j, i)=&
+            (tzd1(k)*w(k-1, j, i)*(w(k, j, i)+w(k-1, j, i))-tzd2(k)*w(k+1, j, i)*(w(k, j, i)+w(k+1, j, i))) + &
+            (2.0*(w(k, j, i-1)*(u(k, j, i-1)+u(k+1, j, i-1))-w(k, j, i+1)*(u(k, j, i)+u(k+1, j, i)))) + &
+            (2.0*(w(k, j-1, i)*(v(k, j-1, i)+v(k+1, j-1, i))-w(k, j+1, i)*(v(k, j, i)+v(k+1, j, i))))
+        end do
+      end do
+    end do
+end subroutine main
