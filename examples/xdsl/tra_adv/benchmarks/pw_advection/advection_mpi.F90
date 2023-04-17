680bac6359804134229ef87010b7231fe4baf631
Nick Brown <n.brown@epcc.ed.ac.uk>
2023-04-17 16:45:29 +0100

    Timing added to MPI benchmark

--- examples/xdsl/tra_adv/benchmarks/pw_advection/advection_mpi.F90
+++ examples/xdsl/tra_adv/benchmarks/pw_advection/advection_mpi.F90
@@ -1,13 +1,16 @@
 subroutine main()
     real*8, dimension(:,:,:), allocatable :: su, sv, sw, u, v, w
     real*8, dimension(:), allocatable :: tzc1, tzc2, tzd1, tzd2
-    integer :: k, j, i, nx, ny, nz
+    integer :: k, j, i, nx, ny, nz, rank
+    integer :: itimer0, itimer1
 
     call MPI_Init()
-    
-    nx=128
-    ny=128
-    nz=128
+
+    rank=MPI_CommRank()
+
+    nx=256
+    ny=512
+    nz=256
 
     allocate(su(nz, ny, nx))
     allocate(sv(nz, ny, nx))
@@ -20,6 +23,11 @@ subroutine main()
     allocate(tzd1(nz))
     allocate(tzd2(nz))
 
+    if (rank == 0) then
+      call timer_init()
+      call timer_start(itimer0, label='Initialise')
+    end if
+
     do i=1, nx
       do j=1, ny
         do k=1, nz
@@ -29,7 +37,7 @@ subroutine main()
         end do
       end do
     end do
-    
+
     do k=1, nz
       tzc1(k)=50.0
       tzc2(k)=15.0
@@ -37,13 +45,18 @@ subroutine main()
       tzd2(k)=5.0
     end do
 
+    if (rank == 0) then
+      call timer_stop(itimer0)
+      call timer_start(itimer1, label='Compute')
+    end if
+
     do i=2,nx-1
       do j=2,ny-1
         do k=2,nz-1
           su(k, j, i)=&
             (2.0*(u(k, j, i-1)*(u(k, j, i)+u(k, j, i-1))-u(k, j, i+1)*(u(k, j, i)+u(k, j, i+1)))) + &
             (1.0*(u(k, j-1, i)*(v(k, j-1, i)+v(k, j-1, i+1))-u(k, j+1, i)*(v(k, j, i)+v(k, j, i+1)))) + &
-            (tzc1(k)*u(k-1, j, i)*(w(k-1, j, i)+w(k-1, j, i+1))-tzc2(k)*u(k+1, j, i)*(w(k, j, i)+w(k, j, i+1)))   
+            (tzc1(k)*u(k-1, j, i)*(w(k-1, j, i)+w(k-1, j, i+1))-tzc2(k)*u(k+1, j, i)*(w(k, j, i)+w(k, j, i+1)))
 
           sv(k, j, i)=&
             (2.0*(v(k, j-1, i)*(v(k, j, i)+v(k, j-1, i))-v(k, j+1, i)*(v(k, j, i)+v(k, j+1, i)))) + &
@@ -53,10 +66,15 @@ subroutine main()
           sw(k, j, i)=&
             (tzd1(k)*w(k-1, j, i)*(w(k, j, i)+w(k-1, j, i))-tzd2(k)*w(k+1, j, i)*(w(k, j, i)+w(k+1, j, i))) + &
             (2.0*(w(k, j, i-1)*(u(k, j, i-1)+u(k+1, j, i-1))-w(k, j, i+1)*(u(k, j, i)+u(k+1, j, i)))) + &
-            (2.0*(w(k, j-1, i)*(v(k, j-1, i)+v(k+1, j-1, i))-w(k, j+1, i)*(v(k, j, i)+v(k+1, j, i))))       
+            (2.0*(w(k, j-1, i)*(v(k, j-1, i)+v(k+1, j-1, i))-w(k, j+1, i)*(v(k, j, i)+v(k+1, j, i))))
         end do
       end do
     end do
+
+    if (rank == 0) then
+      call timer_stop(itimer1)
+      call timer_report()
+    end if
 
     deallocate(su)
     deallocate(sv)
